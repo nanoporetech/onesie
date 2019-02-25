4de5ec4d066cacd75d05582b495bc20f4365aa11
Richard Crewe <richard.crewe@nanoporetech.com>
2019-02-25 17:19:44 +0000

    CORE-1221 Fix hex input bug

--- utils/minit-eeprom.cpp
+++ utils/minit-eeprom.cpp
@@ -86,7 +86,7 @@ int main(int argc, char* argv[]) {
             ++index;
             std::string field(argv[index]);
             try {
-                start = (unsigned int)std::stoul(field);
+                start = (unsigned int)std::stoul(field,0);
             } catch(std::invalid_argument& e) {
                 std::cerr << "couldn't convert '" << field << "'to a number" << std::endl;
                 exit(1);
@@ -101,7 +101,7 @@ int main(int argc, char* argv[]) {
             ++index;
             std::string field(argv[index]);
             try {
-                length = (unsigned int)std::stoul(field);
+                length = (unsigned int)std::stoul(field,0);
             } catch(std::invalid_argument& e) {
                 std::cerr << "couldn't convert '" << field << "'to a number" << std::endl;
                 exit(1);
