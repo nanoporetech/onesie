672f2caef792e8a1ff3e363faea06dc6cfa91583
Richard Crewe <richard.crewe@nanoporetech.com>
2023-02-14 16:38:33 +0000

    Move Debian packaging commands into a script

--- Makefile
+++ Makefile
@@ -1,5 +1,4 @@
 # default to distributing as source and relying on DKMS to build
-COMPILED_DRIVER_PACKAGE ?= 0
 PACKAGE_BASE_NAME := ont-minion1c-driver
 
 # DKMS sets KERNELRELEASE
@@ -68,69 +67,16 @@ clean:
 	$(RM) $(PACKAGE_BASE_NAME)_*.tar.gz
 
 .PHONY: dist-deb
-.ONESHELL: # The rules execute in a single shell (as opposed to one shell per line.)
 dist-deb:
-	set -e
-	# assmeble all the files under package
-	rm -rf package
-	mkdir -p package/debian
-	# make the .deb control file and change kernel verison number
-	cp debian/control package/debian/control
-	if [ $(COMPILED_DRIVER_PACKAGE) -eq 1 ]; then
-		sed -e "s/_KVERS_/$(KVERS)/g;s/_VERSION_/$(VERSION)/g;s/_FIRMWARE-VERSION_/$(FIRMWARE_VERSION)/g" debian/control.modules.in >> package/debian/control
-		sed -e "s/_KVERS_/$(KVERS)/g" debian/postinst.modules.in > package/debian/ont-minion1c-driver-$(KVERS).postinst
-		sed -e "s/_KVERS_/$(KVERS)/g" debian/postrm.modules.in > package/debian/ont-minion1c-driver-$(KVERS).postrm
-	fi
-	sed -i -e "s/_ARCH_/$(DEB_ARCH)/g;s/_VERSION_/$(VERSION)/g;s/_FIRMWARE-VERSION_/$(FIRMWARE_VERSION)/g" package/debian/control
-	# debhelper version-9
-	echo 9 > package/debian/compat
-	echo "VERSION_SUFFIX=$(VERSION_SUFFIX)"
+	debian/debian_packaging.sh \
+		--version $(VERSION)\
+		--debian_revision $(DEBIAN_REVISION)\
+		--version-suffix $(VERSION_SUFFIX)\
+		--kernel $(KVERS)\
+		--firmware $(FIRMWARE_VERSION)\
+		--architecture $(DEB_ARCH)\
+		--package-base-name $(PACKAGE_BASE_NAME)\
+		$(if $(COMPILED_DRIVER_PACKAGE),--compiled-module)
 
-	# Create a changelog. (Specialise the template.)
-	cp debian/changelog.template package/debian/changelog
-	sed -e "s/@NAME@/$(PACKAGE_BASE_NAME)/g" -i package/debian/changelog
-	sed -e "s/@VERSION@/$(VERSION)-$(DEBIAN_REVISION)$(VERSION_SUFFIX)/g" -i package/debian/changelog
-	sed -e "s/@DATE@/$$(date -R)/g" -i package/debian/changelog
-
-	# copy the source and packaging information, make the source package
-	mkdir $(PACKAGE_BASE_NAME)-$(VERSION)
-	cp -r package/debian $(PACKAGE_BASE_NAME)-$(VERSION)
-	cp -r driver utils Makefile $(PACKAGE_BASE_NAME)-$(VERSION)
-	dpkg-source --build $(PACKAGE_BASE_NAME)-$(VERSION)
-	$(RM) -r $(PACKAGE_BASE_NAME)-$(VERSION)
-
-	# cleanup
-	(cd package && fakeroot dh_prep)
-
-	# if this is a binary then make and add driver object file.
-	if [ $(COMPILED_DRIVER_PACKAGE) -eq 1 ]; then
-		$(MAKE) -C driver DESTDIR=$(PWD)/package/debian/$(PACKAGE_BASE_NAME)-$(KVERS) PREFIX=/usr install-modules
-	fi
-	# add driver-includes, add source files for DKMS
-	$(MAKE) -C driver DESTDIR=$(PWD)/package/debian/$(PACKAGE_BASE_NAME)-dev PREFIX=/usr install-dev
-	$(MAKE) -C driver distdir=$(PWD)/package/debian/$(PACKAGE_BASE_NAME)-dkms/usr/src/$(PACKAGE_BASE_NAME)-$(VERSION) dist
-	$(MAKE) -C udev DESTDIR=$(PWD)/package/debian/$(PACKAGE_BASE_NAME)-udev install
-	$(MAKE) -C utils VERSION=$(VERSION) DESTDIR=$(PWD)/package/debian/$(PACKAGE_BASE_NAME)-utils PREFIX=/usr install
-	# change the DKMS version to match the driver
-	sed -e "s/_VERSION_/$(VERSION)/g" debian/dkms.conf.in > package/debian/$(PACKAGE_BASE_NAME)-dkms.dkms
-
-	cd package
-	# generate .deb files to install binary driver
-	if [ $(COMPILED_DRIVER_PACKAGE) -eq 1 ]; then
-		fakeroot dh_installmodules
-	fi
-	# generate .deb files for the rest
-	fakeroot dh_strip --no-automatic-dbgsym
-	fakeroot dh_installchangelogs
-	fakeroot dh_dkms -p $(PACKAGE_BASE_NAME)-dkms
-	fakeroot dh_makeshlibs
-	fakeroot dh_shlibdeps
-	fakeroot dh_installdeb
-	fakeroot dh_gencontrol
-	fakeroot dh_compress
-	fakeroot dh_md5sums
-	fakeroot dh_builddeb
-	cd ..
-	$(RM) -r package
 
 .PHONY: all install driver utils clean
---
+++ debian/debian_packaging.sh
@@ -0,0 +1,134 @@
+#! /bin/bash -e
+
+function make_lintian_override_if_needed() {
+    local package_name="$1"
+
+    # The overrides file is needed due to a reported error about virtual
+    # packages specified with a version number. This is thought to be a bug in
+    # Lintian, later version of Lintian only complain if the version is
+    # specified with a greater-than or less-than.
+    if dpkg --compare-versions "$(lintian --version | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+.*$')" "gt" "2.5.43"
+    then
+        return
+    fi
+
+    sed -e "s#PACKAGE#${package_name}#g" \
+        "${PWD}/debian/lintian-overrides" > "${PWD}/package/debian/${package_name}.lintian-overrides"
+}
+
+# expect to be run from the base directory
+echo "$0"
+if [ "$0" != "debian/debian_packaging.sh" ]; then
+    echo "expect to be run from the base directory of the onsie project"
+    exit 1
+fi
+
+# process arguments
+while [ -n "$1" ] ;do
+    case "$1" in
+    "--version")
+        VERSION="$2"
+        shift 2
+        ;;
+    "--debian_revision")
+        DEBIAN_REVISION="$2"
+        shift 2
+        ;;
+    "--version-suffix")
+        VERSION_SUFFIX="$2"
+        shift 2
+        ;;
+    "--kernel")
+        KVERS="$2"
+        shift 2
+        ;;
+    "--firmware")
+        FIRMWARE_VERSION="$2"
+        shift 2
+        ;;
+    "--architecture")
+        DEB_ARCH="$2"
+        shift 2
+        ;;
+    "--package-base-name")
+        PACKAGE_BASE_NAME="$2"
+        shift 2
+        ;;
+    "--compiled-module")
+        COMPILED_DRIVER_PACKAGE=1
+        shift
+        ;;
+    *)
+        echo "Did not recognise option $1"
+        exit 1
+    esac
+done
+
+# assmeble all the files under package
+rm -rf package
+mkdir -p package/debian
+
+# make the .deb control file and change kernel verison number
+cp debian/control package/debian/control
+if [ -n "${COMPILED_DRIVER_PACKAGE}" ]; then
+    sed -e "s/_KVERS_/${KVERS}/g;s/_VERSION_/${VERSION}/g;s/_FIRMWARE-VERSION_/${FIRMWARE_VERSION}/g" debian/control.modules.in >> package/debian/control
+    sed -e "s/_KVERS_/${KVERS}/g" debian/postinst.modules.in > "package/debian/ont-minion1c-driver-${KVERS}.postinst"
+    sed -e "s/_KVERS_/${KVERS}/g" debian/postrm.modules.in > "package/debian/ont-minion1c-driver-${KVERS}.postrm"
+fi
+sed -i -e "s/_ARCH_/${DEB_ARCH}/g;s/_VERSION_/${VERSION}/g;s/_FIRMWARE-VERSION_/${FIRMWARE_VERSION}/g" package/debian/control
+# debhelper version-9
+echo 9 > package/debian/compat
+echo "VERSION_SUFFIX=${VERSION_SUFFIX}"
+
+# Create a changelog. (Specialise the template.)
+cp debian/changelog.template package/debian/changelog
+sed -e "s/@NAME@/${PACKAGE_BASE_NAME}/g" -i package/debian/changelog
+sed -e "s/@VERSION@/${VERSION}-${DEBIAN_REVISION}${VERSION_SUFFIX}/g" -i package/debian/changelog
+sed -e "s/@DATE@/$(date -R)/g" -i package/debian/changelog
+
+# Create lintian-overrides for the package
+make_lintian_override_if_needed "${PACKAGE_BASE_NAME}-dkms"
+
+# copy the source and packaging information, make the source package
+mkdir "${PACKAGE_BASE_NAME}-${VERSION}"
+cp -r package/debian "${PACKAGE_BASE_NAME}-${VERSION}"
+cp -r driver utils Makefile "${PACKAGE_BASE_NAME}-${VERSION}"
+dpkg-source --build "${PACKAGE_BASE_NAME}-${VERSION}"
+rm -r "${PACKAGE_BASE_NAME}-${VERSION}"
+
+# cleanup
+(cd package && fakeroot dh_prep)
+
+# if this is a binary then make and add driver object file.
+if [ -n "${COMPILED_DRIVER_PACKAGE}" ]; then
+    # Create lintian-overrides for the package
+    make_lintian_override_if_needed "${PACKAGE_BASE_NAME}-${KVERS}"
+    make -C driver DESTDIR="${PWD}/package/debian/${PACKAGE_BASE_NAME}-${KVERS}" PREFIX=/usr install-modules
+fi
+# add driver-includes, add source files for DKMS
+make -C driver DESTDIR="${PWD}/package/debian/${PACKAGE_BASE_NAME}-dev" PREFIX=/usr install-dev
+make -C driver distdir="${PWD}/package/debian/${PACKAGE_BASE_NAME}-dkms/usr/src/${PACKAGE_BASE_NAME}-${VERSION}" dist
+make -C udev DESTDIR="${PWD}/package/debian/${PACKAGE_BASE_NAME}-udev" install
+make -C utils VERSION="${VERSION}" DESTDIR="${PWD}/package/debian/${PACKAGE_BASE_NAME}-utils" PREFIX=/usr install
+# change the DKMS version to match the driver
+sed -e "s/_VERSION_/${VERSION}/g" debian/dkms.conf.in > "package/debian/${PACKAGE_BASE_NAME}-dkms.dkms"
+
+cd package
+# generate .deb files to install binary driver
+if [ -n "${COMPILED_DRIVER_PACKAGE}" ]; then
+    fakeroot dh_installmodules
+fi
+# generate .deb files for the rest
+fakeroot dh_strip --no-automatic-dbgsym
+fakeroot dh_lintian
+fakeroot dh_installchangelogs
+fakeroot dh_dkms -p "${PACKAGE_BASE_NAME}-dkms"
+fakeroot dh_makeshlibs
+fakeroot dh_shlibdeps
+fakeroot dh_installdeb
+fakeroot dh_gencontrol
+fakeroot dh_compress
+fakeroot dh_md5sums
+fakeroot dh_builddeb
+cd ..
+#rm -r package
